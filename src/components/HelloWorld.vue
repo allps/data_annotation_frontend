71ea1f1b3123f9cd5298f9d9bb8b9927534713c8
pragati <pandeypragati2112@gmail.com>
2020-05-29 16:56:51 +0530

    initial version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+
+      <div v-for="(item, index) in images"  v-bind:key="item" >
+
+          <img id= "item" class="image-class" :src="getImageUrl(item)"  @click="hideImage(item, index)">
+
+      </div>
+
+
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data(){
+    return{
+          images: [],
+    }
+  },
+
+  mounted() {
+    this.axiosInstance = axios.create({
+      timeout: 50000,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    });
+
+    this.getImages();
+  },
+
+  methods:{
+      getImages(){
+          let pageNumber = window.location.href.split('/').slice(-1)[0];
+        this.axiosInstance.post('/images/' + pageNumber )
+        .then(response =>{
+          this.$data.images = response.data;
+        }).catch(error=>{
+          console.log(error.response.data)
+        })
+      },
+
+      getImageUrl(item){
+          return 'http://127.0.0.1:4000/'+item;
+      },
+      hideImage(item, i){
+          let data = {
+              'bad_image': item
+          };
+          console.log(data);
+          document.getElementsByTagName('img')[i].style.display = 'none';
+          this.axiosInstance.post('/bad-image', data)
+          .then(response=>{
+              console.log(response.data);
+          }).catch(error=>{
+              console.log(error.response.data);
+          })
+      },
   }
+
 }
 </script>
 
@@ -54,5 +79,14 @@ li {
 }
 a {
   color: #42b983;
+}
+.image-class{
+    border: black;
+    width:300px;
+    height:200px;
+    float: left;
+}
+.container{
+    padding: 0rem 3rem 0rem 5rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+window.axios = axios;
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
